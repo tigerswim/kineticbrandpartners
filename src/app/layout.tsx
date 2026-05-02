03354fe4d901d9c1b0da0777b2ed2a754443b3f6
tigerswim <danhoeller@gmail.com>
2026-05-02 13:22:48 -0400

    feat(sbd): add Montserrat font (open-source Proxima Nova substitute per SBD brand)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -154,6 +154,10 @@ export default function RootLayout({
         {/* Google Fonts preconnect for Hanken Grotesk + Figtree */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
 
         {/* Resource hints for external services */}
         <link rel="preconnect" href="https://www.googletagmanager.com" />
